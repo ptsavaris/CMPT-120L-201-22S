d3861cc078d0f70b86a99bcf0dd5f5aa2f3c5b62
ptsavaris <99047872+ptsavaris@users.noreply.github.com>
2022-04-13 18:49:08 -0400

    lab 6

---
+++ JupyterNotebooks/Labs/Lab6.py
@@ -0,0 +1,16 @@
+def leap_year(year):
+    if year % 100 ==0 and year % 400 == 0:
+        return "It is a leap year"
+    elif year % 4 == 0 and not year % 100 == 0:
+        return "It is not a leap year"
+    else:
+        return "It is not a leap year"
+
+
+if __name__ == "__main__":
+    years = [2000, 1994, 1912, 3002, 1700, 1400]
+    answers = []
+    for year in years:
+        answers.append(leap_year(year))
+    
+    print(answers)
